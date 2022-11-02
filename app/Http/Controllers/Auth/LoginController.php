56a86c0e055ca394a210ce34fad0eeaf005b4dd6
hp <hp@Kratika-lp>
2022-11-02 15:46:19 +0530

    Added last_login date and status on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,6 +30,8 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    
+     protected $user;
 
     /**
      * Create a new controller instance.
@@ -36,5 +41,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    
+    function authenticated(Request $request, $user)
+    {
+        $user->update([
+            'last_login' => Carbon::now()->toDateTimeString(),
+            'status' => true
+        ]);
+        $this->user = Auth::user();
+    }
+  
+   function loggedOut(Request $request)
+    {
     }
 }
